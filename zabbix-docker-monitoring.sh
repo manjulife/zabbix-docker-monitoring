603fd92159a3cee2cd05e2b2290411326f5c0a1e
manjulife <vicmanjul@gmail.com>
2019-06-26 13:54:02 +0300

    add state all containers

--- zabbix-docker-monitoring.sh
+++ zabbix-docker-monitoring.sh
@@ -1,10 +1,9 @@
 #!/bin/bash 
-containerID=$(curl -s --unix-socket /var/run/docker.sock http:/localhost/containers/json | jq -r ".[] | select(.Names[0]==\"/heuristic_engelbart\") | .Id")
+containerID=$(curl -s --unix-socket /var/run/docker.sock http:/localhost/containers/json?all=1 | jq -r ".[] | select(.Names[0]==\"/heuristic_engelbart\") | .Id")
 echo "containerID: $containerID"
 containerState=$(curl -s --unix-socket /var/run/docker.sock http:/localhost/containers/$containerID/json | jq -r .State.Status )
-
-
 echo "state: $containerState"
+
 #for name in $namesContainer; do
 #  name=${name:1}
   
