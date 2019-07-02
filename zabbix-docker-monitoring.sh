b2be21385d9667063daf61707438b373a3ab93a2
manjulife <vicmanjul@gmail.com>
2019-07-02 18:35:55 +0300

    debug params

--- zabbix-docker-monitoring.sh
+++ zabbix-docker-monitoring.sh
@@ -1,28 +1,49 @@
 #!/bin/bash
-containerName=$1
-#metricaName=$2
-#isDebug=$3
+paramCName=$1
+paramDebug=$2
 
-#if echo "$metricaName"
-
-#if [ "$metricaName" == "status" ]
-#then
-#elif [ "$metricaName" == "disk" ]
-
-containerID=$(curl -s --unix-socket /var/run/docker.sock http:/localhost/containers/json?all=1 | jq -r ".[] | select(.Names[0]==\"/$containerName\") | .Id")
-#echo "containerID: $containerID"
-containerState=$(curl -s --unix-socket /var/run/docker.sock http:/localhost/containers/$containerID/json | jq -r .State.Status )
-#echo "state: $containerState"
-
-if [ "running" == "$containerState" ]
+if [ -n "$paramDebug" ] && [ "$paramDebug" -eq 1 ]
 then
-    echo "1"
-elif [ "exited" == "$containerState" ]
-then
-    echo "2"
+   isDebug=1
 else
-    echo "0"
+   isDebug=0
 fi
+
+function container_status {
+
+    argCName=$1
+    argIsDebug=$2
+
+    if [ "$argIsDebug" -eq 1 ]
+    then
+       echo "containerName: $argCName"
+    fi
+
+    containerID=$(curl -s --unix-socket /var/run/docker.sock http:/localhost/containers/json?all=1 | jq -r ".[] | select(.Names[0]==\"/$argCName\") | .Id")
+    if [ "$argIsDebug" -eq 1 ]
+    then
+       echo "containerID: $containerID"
+    fi
+
+    containerState=$(curl -s --unix-socket /var/run/docker.sock http:/localhost/containers/$containerID/json | jq -r .State.Status )
+    if [ "$argIsDebug" -eq 1 ]
+    then
+       echo "state: $containerState"
+    fi
+    # result
+    if [ "running" == "$containerState" ]
+    then
+        echo "5"
+    elif [ "exited" == "$containerState" ]
+    then
+        echo "1"
+    else
+        # other result
+        echo "0"
+    fi
+}
+
+container_status "$paramCName" "$isDebug"
 
 #   created 
 #   running
@@ -31,4 +52,3 @@ fi
 #   exited
 #   paused
 #   dead
-
